630bd834758409ad0de557837ae40915a5928910
Paul Stupka <psk@sitecore.net>
2021-09-04 16:14:13 +0300

    Initialize the project

---
+++ src/Sitecore.Support.496570/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.496570")]
+[assembly: AssemblyProduct("Sitecore.Support.496570")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Cumulative patch for Azure Search issues in Sitecore 9.0 Update-2")]
