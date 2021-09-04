a0cc76814a8fb80e293eb3ee9d578fc769634d14
Paul Stupka <psk@sitecore.net>
2021-09-04 17:17:26 +0300

    Fix issue 299679

--- src/Sitecore.Support.496570/ContentSearch/Azure/CloudSearchIndexOperations.cs
+++ src/Sitecore.Support.496570/ContentSearch/Azure/CloudSearchIndexOperations.cs
@@ -2,10 +2,15 @@
 
 namespace Sitecore.Support.ContentSearch.Azure
 {
-    public class CloudSearchIndexOperations : Sitecore.ContentSearch.Azure.CloudSearchIndexOperations
+    public class CloudSearchIndexOperations : Sitecore.ContentSearch.Azure.CloudSearchIndexOperations, IIndexOperations
     {
         public CloudSearchIndexOperations(ISearchIndex index) : base(index)
         {
+        }
+
+        void IIndexOperations.Delete(IIndexable indexable, IProviderUpdateContext context)
+        {
+            context.Delete(indexable.UniqueId);
         }
     }
 }
